f2bcc9b8a02aef0a6b2db4a212370b00e66fdfaa
Sarmient02 <javiersarmiento02@gmail.com>
2022-09-13 00:08:51 -0500

    Update TipoDocumento.java

--- personaBACKEND/src/main/java/com/example/personaBACKEND/model/TipoDocumento.java
+++ personaBACKEND/src/main/java/com/example/personaBACKEND/model/TipoDocumento.java
@@ -1,15 +1,44 @@
 package com.example.personaBACKEND.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "tipodocumento")
 public class TipoDocumento {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String nombre;
     private String descripcion;
 
+
+    public TipoDocumento(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
