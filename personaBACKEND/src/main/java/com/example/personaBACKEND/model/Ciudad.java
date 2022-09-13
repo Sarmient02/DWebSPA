037790a3d46e0624f1f2f99ba18914ec105addf1
Sarmient02 <javiersarmiento02@gmail.com>
2022-09-13 00:08:48 -0500

    Update Ciudad.java

--- personaBACKEND/src/main/java/com/example/personaBACKEND/model/Ciudad.java
+++ personaBACKEND/src/main/java/com/example/personaBACKEND/model/Ciudad.java
@@ -1,15 +1,42 @@
 package com.example.personaBACKEND.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ciudad")
 public class Ciudad {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String nombre;
     private String descripcion;
 
+    public Ciudad(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
