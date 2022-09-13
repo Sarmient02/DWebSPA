b1bd5a5f2ec20dfb8c5807a475f40715c4473438
Javier Sarmiento <javiersarmiento02@gmail.com>
2022-09-13 11:42:59 -0500

    Create TipoDocumentoRepository.java

---
+++ PersonaBACKEND/src/main/java/com/example/personaBACKEND/repository/TipoDocumentoRepository.java
@@ -0,0 +1,7 @@
+package com.example.personaBACKEND.repository;
+
+import com.example.personaBACKEND.model.TipoDocumento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoDocumentoRepository extends JpaRepository<TipoDocumento, Long> {
+}
