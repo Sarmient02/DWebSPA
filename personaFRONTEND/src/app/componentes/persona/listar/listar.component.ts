90a7b144f028ba7bd5f50755be8c72ae72f5177f
Sarmient02 <javiersarmiento02@gmail.com>
2022-09-20 00:34:46 -0500

    Update listar.component.ts

--- personaFRONTEND/src/app/componentes/persona/listar/listar.component.ts
+++ personaFRONTEND/src/app/componentes/persona/listar/listar.component.ts
@@ -13,6 +13,8 @@ import { Router } from '@angular/router';
 
 export class ListarComponent implements OnInit {
 
+  reverse:any;
+
   personas: any;
   ciudades: any;
   documentos: any;
@@ -44,6 +46,87 @@ export class ListarComponent implements OnInit {
         this.personas.pop(persona)
       }
     })
+  }
+
+  sortID(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> b.id - a.id)
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> a.id - b.id)
+      this.reverse = !this.reverse;
+    }
+  }
+
+  sortName(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> a.nombre.localeCompare(b.nombre))
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> b.nombre.localeCompare(a.nombre))
+      this.reverse = !this.reverse;
+    }
+  }
+
+  sortTipoDocumento(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> a.tipodocumento.nombre.localeCompare(b.tipodocumento.nombre))
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> b.tipodocumento.nombre.localeCompare(a.tipodocumento.nombre))
+      this.reverse = !this.reverse;
+    }
+  }
+
+  sortDocumento(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> b.documento - a.documento)
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> a.documento - b.documento)
+      this.reverse = !this.reverse;
+      console.log(this.personas);
+    }
+  }
+
+  sortLugarResidencia(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> a.lugarresidencia.nombre.localeCompare(b.lugarresidencia.nombre))
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> b.lugarresidencia.nombre.localeCompare(a.lugarresidencia.nombre))
+      this.reverse = !this.reverse;
+    }
+  }
+
+  sortEmail(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> a.email.localeCompare(b.email))
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> b.email.localeCompare(a.email))
+      this.reverse = !this.reverse;
+    }
+  }
+
+  sortTelefono(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> b.telefono - a.telefono)
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> a.telefono - b.telefono)
+      this.reverse = !this.reverse;
+    }
+  }
+
+  sortUsuario(){
+    if(!this.reverse){
+      this.personas = this.personas.sort((a:any,b:any)=> a.username.localeCompare(b.username))
+      this.reverse = !this.reverse;
+    } else {
+      this.personas = this.personas.sort((a:any,b:any)=> b.username.localeCompare(a.username))
+      this.reverse = !this.reverse;
+    }
   }
   
 }
